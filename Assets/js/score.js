b4513e88230afc2f6ecf1fe74943266bab9a19de
Darrell Markham <dnlmarkham@sbcglobal.net>
2022-01-19 07:24:33 -0800

    added score.js

--- Assets/js/score.js
+++ Assets/js/score.js
@@ -1,4 +1,24 @@
 function printHighScores() {
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach(function(score) {
+        var liTag = document.createElement("li");
+        liTag.textCpntent=score.initials + "-" + score.score;
+
+        var olEl=document.getElementById("highscores");
+        0lEl.appendChild(liTag);
+    })
 }
+
+function clearHighScore() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+}
+
+document.getElementById("clear").onclick=clearHighScores;
+
+printHighScores();
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,9 @@ var timerEl = document.querySelector("#timer");
 var questionsEl = document.querySelector("#questions");
 var answersEl = document.querySelector("#answers");
 var startBtn = document.querySelector("#start");
+var answersEl = document.querySelector("#answers");
+var submitBtn = document.querySelector("#submit");
+var initialsEl = document.querySelector("#initials")
 
 function startQuiz() {
     var homePageEL = document.getElementById("home-page");
